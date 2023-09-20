df06620ee203bddc3d850fa80df75f4ccfd4ba8d
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-09-20 13:54:26 +0100

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * swap - swap two value
+ * @a: first value
+ * @b: second value
+*/
+
+void swap(int *a, int *b)
+{
+	(*a) = (*a) + (*b);
+	(*b) = (*a) - (*b);
+	(*a) = (*a) - (*b);
+}
+
+/**
+ *
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i) 
+		{
+			swap(array + i, array + min);
+			print_array(array, size);
+		}
+	}
+}
